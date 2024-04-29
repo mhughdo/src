e287f4f71f955539e30cb6af7f397ef214880500
Hugh Do <mhughdo@gmail.com>
2024-04-30 02:35:58 +0900

    feat: handle graceful shutdown

--- internal/app/server/server.go
+++ internal/app/server/server.go
@@ -2,7 +2,9 @@ package server
 
 import (
 	"context"
+	"errors"
 	"fmt"
+	"io"
 	"log/slog"
 	"net"
 )
@@ -16,8 +18,9 @@ type Config struct {
 }
 
 type Server struct {
-	cfg Config
-	ln  net.Listener
+	cfg  Config
+	ln   net.Listener
+	done chan struct{}
 }
 
 func NewServer(cfg Config) *Server {
@@ -25,7 +28,8 @@ func NewServer(cfg Config) *Server {
 		cfg.ListenAddr = defaultListenAddr
 	}
 	return &Server{
-		cfg: cfg,
+		cfg:  cfg,
+		done: make(chan struct{}),
 	}
 }
 
@@ -40,15 +44,34 @@ func (s *Server) Listen() error {
 }
 
 func (s *Server) loop() error {
-	c, err := s.ln.Accept()
-	if err != nil {
-		return fmt.Errorf("failed to accept connection: %w", err)
-	}
-	buf := make([]byte, 0, 1024)
 	for {
-		_, err = c.Read(buf)
+		c, err := s.ln.Accept()
 		if err != nil {
+			select {
+			case <-s.done:
+				slog.Info("shutting down listener")
+				return nil
+			default:
+				return fmt.Errorf("failed to accept connection: %w", err)
+			}
+		}
+		go s.handleConn(c)
+	}
+}
+
+func (s *Server) handleConn(c net.Conn) {
+	for {
+		buf := make([]byte, 128)
+		_, err := c.Read(buf)
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				slog.Info("connection closed")
+				return
+			}
 			slog.Error("failed to read command", "err", err)
+			continue
+		}
+		if len(buf) == 0 {
 			continue
 		}
 		slog.Info("received command", "command", string(buf))
@@ -61,8 +84,9 @@ func (s *Server) loop() error {
 }
 
 func (s *Server) Close(_ context.Context) error {
+	close(s.done)
 	if err := s.ln.Close(); err != nil {
-		return err
+		return fmt.Errorf("failed to close listener: %w", err)
 	}
 	return nil
 }
