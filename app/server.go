738723423f7e73bfcb805eb68dd3eaa2d0487be3
Hugh Do <mhughdo@gmail.com>
2024-04-29 17:27:16 +0900

    feat: respond to PING command

--- app/server.go
+++ app/server.go
@@ -1,20 +1,49 @@
 package main
 
 import (
+	"context"
+	"flag"
 	"fmt"
+	"io"
+	"log"
+	"log/slog"
 	"net"
 	"os"
 )
 
-func main() {
-	l, err := net.Listen("tcp", "0.0.0.0:6379")
+var (
+	listen = flag.String("listen", ":6379", "listen address")
+)
+
+func run(_ context.Context, _ io.Writer, _ []string) error {
+	flag.Parse()
+	l, err := net.Listen("tcp", *listen)
+	defer l.Close()
 	if err != nil {
-		fmt.Println("Failed to bind to port 6379")
-		os.Exit(1)
+		return fmt.Errorf("failed to listen: %w", err)
 	}
-	_, err = l.Accept()
+	slog.Info("listening", "port", l.Addr())
+	c, err := l.Accept()
 	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+		return fmt.Errorf("failed to accept connection: %w", err)
+	}
+	defer c.Close()
+	buf := make([]byte, 128)
+	_, err = c.Read(buf)
+	if err != nil {
+		return fmt.Errorf("failed to read command: %w", err)
+	}
+	slog.Info("received command", "command", string(buf))
+	_, err = c.Write([]byte("+PONG\r\n"))
+	if err != nil {
+		return fmt.Errorf("failed to write response: %w", err)
+	}
+
+	return nil
+}
+func main() {
+	ctx := context.Background()
+	if err := run(ctx, os.Stdout, os.Args); err != nil {
+		log.Fatalf("error: %v", err)
 	}
 }
