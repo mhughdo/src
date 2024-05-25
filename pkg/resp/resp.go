7e388b21c8a537ab534680bd4f61fe34a323ba0e
Hugh Do <mhughdo@gmail.com>
2024-05-26 02:56:24 +0900

    feat: implement RESP message parser

---
+++ pkg/resp/resp.go
@@ -0,0 +1,382 @@
+package resp
+
+import (
+	"bufio"
+	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"log/slog"
+	"net"
+	"strconv"
+)
+
+// RESP Data Types
+const (
+	SimpleString   = '+'
+	SimpleError    = '-'
+	Integer        = ':'
+	BulkString     = '$'
+	Array          = '*'
+	Null           = '_'
+	Boolean        = '#'
+	Double         = ','
+	BigNumber      = '('
+	BulkError      = '!'
+	VerbatimString = '='
+	Map            = '%'
+	Set            = '~'
+	Pushes         = '>'
+)
+
+var (
+	ErrUnknownType     = errors.New("unknown type")
+	ErrInvalidType     = errors.New("invalid type")
+	ErrParsingErr      = errors.New("parsing error")
+	ErrIncompleteInput = errors.New("incomplete input")
+	ErrEmptyLine       = errors.New("empty line")
+)
+
+type Resp struct {
+	Length int
+	Type   byte
+	rd     *bufio.Reader
+	Data   any
+}
+
+func NewResp(rd io.Reader) *Resp {
+	return &Resp{
+		rd: bufio.NewReader(rd),
+	}
+}
+
+func (r *Resp) String() string {
+	switch val := r.Data.(type) {
+	case []byte:
+		return string(val)
+	case nil:
+		return ""
+	case []*Resp:
+		if r.Length == -1 {
+			return ""
+		}
+		s, err := serializeResp(val)
+		if err != nil {
+			slog.Error("error serializing array: %v", err)
+			return ""
+		}
+		return s
+	case map[string]*Resp:
+		s, err := serializeResp(val)
+		if err != nil {
+			slog.Error("error serializing map: %v", err)
+			return ""
+		}
+		return s
+	default:
+		return ""
+	}
+}
+
+func (r *Resp) Int() int64 {
+	switch val := r.Data.(type) {
+	case []byte:
+		i, err := strconv.ParseInt(string(val), 10, 64)
+		if err != nil {
+			return 0
+		}
+		return i
+	default:
+		return 0
+	}
+}
+
+func (r *Resp) Float() float64 {
+	switch val := r.Data.(type) {
+	case []byte:
+		f, err := strconv.ParseFloat(string(val), 64)
+		if err != nil {
+			return 0
+		}
+		return f
+	default:
+		return 0
+	}
+}
+
+func (r *Resp) Bool() bool {
+	switch val := r.Data.(type) {
+	case []byte:
+		return string(val) == "t"
+	default:
+		return false
+	}
+}
+
+func (r *Resp) IsNull() bool {
+	return r.Type == Null
+}
+
+func (r *Resp) GetCommandName() (string, error) {
+	if r.Type != Array {
+		return "", fmt.Errorf("invalid type: %c", r.Type)
+	}
+
+	if r.Length <= 0 {
+		return "", fmt.Errorf("invalid length: %d", r.Length)
+	}
+
+	return r.Data.([]*Resp)[0].String(), nil
+}
+
+func (r *Resp) ToStringSlice() ([]string, error) {
+	if r.Type != Array {
+		return nil, fmt.Errorf("invalid type: %c", r.Type)
+	}
+	s := make([]string, r.Length)
+	for i, v := range r.Data.([]*Resp) {
+		s[i] = v.String()
+	}
+	return s, nil
+}
+
+// SimpleString: +OK\r\n
+// SimpleError: -Error message\r\n
+// Integer: :1000\r\n or :-1000\r\n
+// Null: _\r\n
+// Boolean: #t\r\n
+// Double: ,3.14\r\n or ,10\r\n
+// BigNumber: (3492890328409238509324850943850943825024385\r\n
+// BulkString: $5\r\nhello\r\n
+// BulkError: !21\r\nSYNTAX invalid syntax\r\n
+// VerbatimString: =15\r\ntxt:Some string\r\n
+// Array: *2\r\n$5\r\nhello\r\n$5\r\nworld\r\n
+// Map: %2\r\n+first\r\n:1\r\n+second\r\n:2\r\n
+// Set: ~3\r\n+first\r\n+second\r\n+third\r\n
+// Pushes: >2\r\n+first\r\n+second\r\n
+
+func (r *Resp) readLine() ([]byte, error) {
+	b, err := r.rd.ReadSlice('\n')
+	if err != nil {
+		if err != bufio.ErrBufferFull {
+			return nil, err
+		}
+
+		full := make([]byte, len(b))
+		copy(full, b)
+
+		b, err = r.rd.ReadBytes('\n')
+		if err != nil {
+			return nil, err
+		}
+
+		full = append(full, b...)
+		b = full
+	}
+	if len(b) <= 2 || b[len(b)-1] != '\n' || b[len(b)-2] != '\r' {
+		return nil, fmt.Errorf("invalid input: %q", b)
+	}
+	return b[:len(b)-2], nil
+}
+
+func (r *Resp) ParseResp() (*Resp, error) {
+	line, err := r.readLine()
+	if err != nil {
+		return nil, fmt.Errorf("error reading line: %w", err)
+	}
+	if len(line) == 0 {
+		return nil, ErrEmptyLine
+	}
+	respType := line[0]
+	resp := Resp{Type: respType}
+	n := 0
+	switch respType {
+	case SimpleString, SimpleError, Integer, Null, Boolean, Double, BigNumber:
+		resp.Data = line[1:]
+	case BulkString, BulkError, VerbatimString:
+		resp.Data, n, err = r.parseBulkString(line)
+		if err != nil {
+			return nil, fmt.Errorf("error parsing bulk input: %w", err)
+		}
+		if n != -1 && len(resp.Data.([]byte)) != n {
+			return nil, fmt.Errorf("%w: expected length %d, got length %d", ErrIncompleteInput, n, len(resp.Data.([]byte)))
+		}
+	case Array, Pushes, Set:
+		resp.Data, n, err = r.parseArray(line)
+		if err != nil {
+			return nil, fmt.Errorf("error parsing array input: %w", err)
+		}
+		if n != -1 && n != len(resp.Data.([]*Resp)) {
+			return nil, fmt.Errorf("%w: expected length %d, got length %d", ErrIncompleteInput, n, len(resp.Data.([]*Resp)))
+		}
+	case Map:
+		resp.Data, n, err = r.parseMap(line)
+		if err != nil {
+			return nil, fmt.Errorf("error parsing map input: %w", err)
+		}
+		if n != -1 && n != len(resp.Data.(map[string]*Resp)) {
+			return nil, fmt.Errorf("%w: expected length %d, got length %d", ErrIncompleteInput, n, len(resp.Data.(map[string]*Resp)))
+		}
+	default:
+		return nil, fmt.Errorf("%w: %q", ErrUnknownType, respType)
+	}
+
+	resp.Length = n
+	return &resp, nil
+}
+
+func (r *Resp) parseBulkString(line []byte) ([]byte, int, error) {
+	n, err := parseLen(line)
+	if err != nil {
+		return nil, 0, err
+	}
+	if n == -1 {
+		return []byte{}, n, nil
+	}
+	b := make([]byte, n+2)
+	_, err = io.ReadFull(r.rd, b)
+	if err != nil {
+		return []byte{}, n, err
+	}
+	return b[:n], n, nil
+}
+
+// func (r *RESP) parseVerbatimString(line []byte) ([]byte, error) {
+// 	b, err := r.parseBulkString(line)
+// 	if err != nil {
+// 		return []byte{}, err
+// 	}
+// 	if len(b) < 4 || b[3] != ':' {
+// 		return []byte{}, fmt.Errorf("can't parse verbatim string reply: %q", line)
+// 	}
+// 	return b[4:], nil
+// }
+
+func (r *Resp) parseMap(line []byte) (map[string]*Resp, int, error) {
+	n, err := parseLen(line)
+	if err != nil {
+		return nil, 0, err
+	}
+	if n == -1 {
+		return nil, n, nil
+	}
+	m := make(map[string]*Resp, n)
+	for i := 0; i < n; i++ {
+		key, err := r.ParseResp()
+		if err != nil {
+			return nil, n, err
+		}
+		val, err := r.ParseResp()
+		if err != nil {
+			return nil, n, err
+		}
+		m[key.String()] = val
+	}
+	return m, n, nil
+}
+
+func (r *Resp) parseArray(line []byte) ([]*Resp, int, error) {
+	n, err := parseLen(line)
+	if err != nil {
+		return nil, 0, err
+	}
+	if n == -1 {
+		return nil, n, nil
+	}
+	arr := make([]*Resp, n)
+	for i := 0; i < n; i++ {
+		resp, err := r.ParseResp()
+		if err != nil {
+			return nil, n, err
+		}
+		arr[i] = resp
+	}
+	return arr, n, nil
+}
+
+func serializeResp(in any) (string, error) {
+	r, err := json.Marshal(in)
+	if err != nil {
+		return "", fmt.Errorf("error marshalling resp: %w", err)
+	}
+
+	return string(r), nil
+}
+
+// func stringifyArray(s []*Resp) (string, error) {
+// 	var b strings.Builder
+// 	b.WriteString("[")
+// 	for i, resp := range s {
+// 		b.WriteString(resp.String())
+// 		if i < len(s)-1 {
+// 			b.WriteString(",")
+// 		}
+// 	}
+// 	b.WriteString("]")
+// 	return b.String(), nil
+// }
+
+// func stringifyMap(m map[string]*Resp) (string, error) {
+// 	var b strings.Builder
+// 	b.WriteString("{")
+// 	i := 0
+// 	for k, v := range m {
+// 		b.WriteString(k)
+// 		b.WriteString(":")
+// 		b.WriteString(v.String())
+// 		if i < len(m)-1 {
+// 			b.WriteString(",")
+// 		}
+// 		i++
+// 	}
+// 	b.WriteString("}")
+// 	return b.String(), nil
+// }
+
+func parseLen(line []byte) (int, error) {
+	n, err := strconv.Atoi(string(line[1:]))
+	if err != nil {
+		return 0, err
+	}
+	if n < -1 {
+		return 0, fmt.Errorf("error parsing length: invalid length %d", n)
+	}
+	return n, nil
+}
+
+func ErrRespone(conn net.Conn, buffer *bytes.Buffer, msg string) {
+	slog.Error(msg)
+	buffer.Reset()
+	r := &Resp{
+		Type:   BulkError,
+		Data:   []byte(msg),
+		Length: len(msg),
+	}
+	conn.Write(r.ToResponse())
+}
+
+func (r *Resp) ToResponse() []byte {
+	switch r.Type {
+	case SimpleString, SimpleError, Integer, Null, Boolean, Double, BigNumber:
+		return []byte(fmt.Sprintf("%c%s\r\n", r.Type, r.Data))
+	case BulkString, VerbatimString:
+		return []byte(fmt.Sprintf("%c%d\r\n%s\r\n", r.Type, r.Length, r.Data))
+	case BulkError:
+		return []byte(fmt.Sprintf("%c%d\r\nERR %s\r\n", r.Type, r.Length, r.Data))
+	case Array, Pushes, Set:
+		b := []byte(fmt.Sprintf("%c%d\r\n", r.Type, r.Length))
+		for _, resp := range r.Data.([]*Resp) {
+			b = append(b, resp.ToResponse()...)
+		}
+	case Map:
+		b := []byte(fmt.Sprintf("%c%d\r\n", r.Type, r.Length))
+		for k, v := range r.Data.(map[string]*Resp) {
+			b = append(b, []byte(k)...)
+			b = append(b, ':')
+			b = append(b, v.ToResponse()...)
+		}
+	}
+
+	return []byte{}
+}
---
+++ pkg/resp/resp_test.go
@@ -0,0 +1,220 @@
+package resp
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestParseRESP(t *testing.T) {
+	t.Parallel()
+	tests := []struct {
+		name    string
+		input   []byte
+		want    *Resp
+		wantErr error
+	}{
+		{
+			name:  "SimpleString",
+			input: []byte("+OK\r\n"),
+			want: &Resp{Type: SimpleString,
+				Data: []byte("OK")},
+			wantErr: nil,
+		},
+		{
+			name:  "SimpleError",
+			input: []byte("-ERR unknown command\r\n"),
+			want: &Resp{Type: SimpleError,
+				Data: []byte("ERR unknown command")},
+			wantErr: nil,
+		},
+		{
+			name:  "Integer",
+			input: []byte(":1000\r\n"),
+			want: &Resp{Type: Integer,
+
+				Data: []byte("1000")},
+			wantErr: nil,
+		},
+		{
+			name:  "Negative Integer",
+			input: []byte(":-1000\r\n"),
+			want: &Resp{Type: Integer,
+
+				Data: []byte("-1000")},
+			wantErr: nil,
+		},
+		{
+			name:  "Null",
+			input: []byte("_\r\n"),
+			want:  &Resp{Type: Null},
+		},
+		{
+			name:  "Boolean",
+			input: []byte("#t\r\n"),
+			want: &Resp{Type: Boolean,
+				Data: []byte("t")},
+		},
+		{
+			name:  "Float",
+			input: []byte(",3.14\r\n"),
+			want: &Resp{Type: Double,
+				Data: []byte("3.14")},
+			wantErr: nil,
+		},
+		{
+			name:  "Infinity Float",
+			input: []byte(",inf\r\n"),
+			want: &Resp{Type: Double,
+				Data: []byte("inf")},
+			wantErr: nil,
+		},
+		{
+			name:  "NaN Float",
+			input: []byte(",nan\r\n"),
+			want: &Resp{Type: Double,
+				Data: []byte("nan")},
+			wantErr: nil,
+		},
+		{
+			name:  "BigNumber",
+			input: []byte("(3492890328409238509324850943850943825024385\r\n"),
+			want: &Resp{Type: BigNumber,
+
+				Data: []byte("3492890328409238509324850943850943825024385")},
+			wantErr: nil,
+		},
+		{
+			name:  "BulkString",
+			input: []byte("$5\r\nhello\r\n"),
+			want: &Resp{Type: BulkString,
+
+				Data: []byte("hello")},
+			wantErr: nil,
+		},
+		{
+			name:  "Null BulkString",
+			input: []byte("$-1\r\n"),
+			want: &Resp{Type: BulkString,
+
+				Data: []byte("")},
+			wantErr: nil,
+		},
+		{
+			name:  "BulkError",
+			input: []byte("!21\r\nSYNTAX invalid syntax\r\n"),
+			want: &Resp{Type: BulkError,
+
+				Data: []byte("SYNTAX invalid syntax")},
+			wantErr: nil,
+		},
+		{
+			name:  "VerbatimString",
+			input: []byte("=15\r\ntxt:Some string\r\n"),
+			want: &Resp{Type: VerbatimString,
+
+				Data: []byte("txt:Some string")},
+			wantErr: nil,
+		},
+		{
+			name:  "Nested Array",
+			input: []byte("*2\r\n*3\r\n:1\r\n:2\r\n:3\r\n*2\r\n+Hello\r\n-World\r\n"),
+			want: &Resp{Type: Array,
+				Data: []*Resp{
+					{Type: Array,
+						Length: 3,
+						Data: []*Resp{
+							{Type: Integer, Data: []byte("1")},
+							{Type: Integer, Data: []byte("2")},
+							{Type: Integer, Data: []byte("3")},
+						},
+					},
+					{
+						Type:   Array,
+						Length: 2,
+						Data: []*Resp{
+							{Type: SimpleString, Data: []byte("Hello")},
+							{Type: SimpleError, Data: []byte("World")},
+						},
+					},
+				},
+			},
+		},
+		{
+			name:  "Array of Mixed Types",
+			input: []byte("*5\r\n:1\r\n:2\r\n:3\r\n:4\r\n$5\r\nhello\r\n"),
+			want: &Resp{Type: Array,
+				Length: 5,
+				Data: []*Resp{
+					{Type: Integer, Data: []byte("1")},
+					{Type: Integer, Data: []byte("2")},
+					{Type: Integer, Data: []byte("3")},
+					{Type: Integer, Data: []byte("4")},
+					{Type: BulkString,
+						Length: 5,
+						Data:   []byte("hello")},
+				},
+			},
+		},
+		{
+			name:  "Array of SimpleString",
+			input: []byte("*2\r\n$5\r\nhello\r\n$5\r\nworld\r\n"),
+			want: &Resp{Type: Array,
+				Length: 2,
+				Data: []*Resp{
+					{Type: BulkString, Length: 5, Data: []byte("hello")},
+
+					{Type: BulkString, Length: 5, Data: []byte("world")},
+				},
+			},
+			wantErr: nil,
+		},
+		{
+			name:  "Array of Integer",
+			input: []byte("*3\r\n:1\r\n:2\r\n:3\r\n"),
+			want: &Resp{Type: Array,
+				Data: []*Resp{
+					{Type: Integer, Data: []byte("1")},
+					{Type: Integer, Data: []byte("2")},
+					{Type: Integer, Data: []byte("3")},
+				},
+			},
+			wantErr: nil,
+		},
+		{
+			name:    "Empty Array",
+			input:   []byte("*0\r\n"),
+			want:    &Resp{Type: Array, Data: []*Resp{}},
+			wantErr: nil,
+		},
+		{
+			name:    "Null Array",
+			input:   []byte("*-1\r\n"),
+			want:    &Resp{Type: Array, Data: nil},
+			wantErr: nil,
+		},
+		{
+			name:  "Map",
+			input: []byte("%2\r\n+first\r\n:1\r\n+second\r\n:2\r\n"),
+			want: &Resp{Type: Map,
+				Length: 2,
+				Data: map[string]*Resp{
+					"first":  {Type: Integer, Data: []byte("1")},
+					"second": {Type: Integer, Data: []byte("2")},
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			resp := NewResp(bytes.NewReader(tt.input))
+			got, err := resp.ParseResp()
+			if err != tt.wantErr {
+				t.Errorf("ParseRESP() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got.String() != tt.want.String() {
+				t.Errorf("ParseRESP() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
